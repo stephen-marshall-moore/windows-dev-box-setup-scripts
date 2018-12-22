0119368af9930282c274eeea54312f3c31e7fa19
stephen-marshall-moore <stephen.moore@linux.com>
2018-12-21 16:41:25 -0800

    Update StephensDevTools.ps1

--- scripts/StephensDevTools.ps1
+++ scripts/StephensDevTools.ps1
@@ -1,4 +1,6 @@
 
 # tools we expect devs across many scenarios will want
-choco install -y git --package-parameters="'/GitOnlyOnPath' '/NoShellIntegration'"
-choco install -y psexec
+choco install git --package-parameters="'/GitOnlyOnPath' '/NoShellIntegration'"
+choco install sysinternals
+choco install sublimetext3
+choco install googlechrome
