9ae8f33812e47ffbffc84615c0df5f78ae360fe1
stephen-marshall-moore <stephen.moore@linux.com>
2018-12-26 11:46:08 -0800

    Update PrepareVM.ps1
    comment reason for ignore exit code

--- scripts/PrepareVM.ps1
+++ scripts/PrepareVM.ps1
@@ -4,4 +4,5 @@
 #Enable-MicrosoftUpdate
 Install-WindowsUpdate -eula
 #Disable-MicrosoftUpdate
+# exit code 3010 indicates success but need for reboot 
 choco install --ignore-package-exit-codes vmware-tools
