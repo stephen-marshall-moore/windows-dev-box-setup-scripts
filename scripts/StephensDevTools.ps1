13f0e814d66297b16ca9cb69504a404ee51cc29c
stephen-marshall-moore <stephen.moore@linux.com>
2018-12-26 12:05:50 -0800

    Update StephensDevTools.ps1

--- scripts/StephensDevTools.ps1
+++ scripts/StephensDevTools.ps1
@@ -4,3 +4,5 @@ choco install git --package-parameters="'/GitOnlyOnPath' '/NoShellIntegration'"
 choco install sysinternals
 choco install sublimetext3
 choco install googlechrome
+# visual studio
+choco install visualstudio2017professional --package-parameters "--allWorkloads --includeRecommended --locale en-US"
