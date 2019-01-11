e14f6d667d905962a03c4303cf252fe9c35bd913
stephen-marshall-moore <stephen.moore@linux.com>
2019-01-11 14:41:59 -0800

    specify folder as destination

--- scripts/vmconfig.ps1
+++ scripts/vmconfig.ps1
@@ -1,5 +1,5 @@
 Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\cylroot.pem'  -CertStoreLocation 'Cert:\LocalMachine\Root'
 Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\root_cylance.cer'  -CertStoreLocation 'Cert:\LocalMachine\Root'
 Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\star_cylance_com.cer'  -CertStoreLocation 'Cert:\LocalMachine\Root'
-Copy-Item -Path '\\vmware-host\Shared Folders\vmconfig\.ssh\*'  -Destination '\Users\stephen\.ssh'
+Copy-Item -Path '\\vmware-host\Shared Folders\vmconfig\.ssh\*'  -Destination '\Users\stephen\.ssh\'
  
