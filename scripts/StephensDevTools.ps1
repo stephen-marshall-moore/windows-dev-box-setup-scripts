156ca8158141c64730f1683dfcefeb6a962cab98
Stephen Moore <smoore@cylance.com>
2019-08-09 15:57:47 -0700

    download configs, more generic home path

--- scripts/StephensDevTools.ps1
+++ scripts/StephensDevTools.ps1
@@ -1,5 +1,5 @@
 
 # tools we expect devs across many scenarios will want
-choco install "C:\Users\stephen\chocoVM.config"
+choco install $Env:HomePath\chocoVM.config
 # visual studio
-# choco install visualstudio2017professional --passive
+choco install visualstudio2017professional --passive --config $Env:HomePath\.vsconfig
--- scripts/vmconfig.ps1
+++ scripts/vmconfig.ps1
@@ -1,8 +1,7 @@
 Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\cylroot.pem'  -CertStoreLocation 'Cert:\LocalMachine\Root'
 Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\root_cylance.cer'  -CertStoreLocation 'Cert:\LocalMachine\Root'
 Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\star_cylance_com.cer'  -CertStoreLocation 'Cert:\LocalMachine\Root'
-New-Item -ItemType 'directory' -Path 'C:\Users\stephen\.ssh'
-Copy-Item -Path '\\vmware-host\Shared Folders\vmconfig\.ssh\*'  -Destination 'C:\Users\stephen\.ssh\'
-Copy-Item -Path '\\vmware-host\Shared Folders\vmconfig\chocoVM.config'  -Destination 'C:\Users\stephen\'
-Copy-Item -Path '\\vmware-host\Shared Folders\vmconfig\studioVM.vsconfig'  -Destination 'C:\Users\stephen\'
- 
+New-Item -ItemType 'directory' -Path $Env:HomePath\.ssh
+Copy-Item -Path '\\vmware-host\Shared Folders\vmconfig\.ssh\*'  -Destination $Env:HomePath\.ssh\
+wget 'https://raw.githubusercontent.com/stephen-marshall-moore/windows-dev-box-setup-scripts/useconfig/config/chocoVM.config'  -OutFile $Env:HomePath\chocoVM.config
+wget 'https://raw.githubusercontent.com/stephen-marshall-moore/windows-dev-box-setup-scripts/useconfig/config/studio2017.vsconfig'  -OutFile $Env:HomePath\.vsconfig
