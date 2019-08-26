f37da511f589bf5399dfa196254d138fef6d8530
stephen-marshall-moore <stephen.moore@linux.com>
2019-08-26 15:00:32 -0700

    add HERA cert so Okta functions

--- scripts/vmconfig.ps1
+++ scripts/vmconfig.ps1
@@ -1,6 +1,7 @@
 Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\cylroot.pem'  -CertStoreLocation 'Cert:\LocalMachine\Root'
 Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\root_cylance.cer'  -CertStoreLocation 'Cert:\LocalMachine\Root'
 Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\star_cylance_com.cer'  -CertStoreLocation 'Cert:\LocalMachine\Root'
+Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\cylancecorp-HERA-CA.cer'  -CertStoreLocation 'Cert:\LocalMachine\Root'
 New-Item -ItemType 'directory' -Path $Env:HomePath\.ssh
 Copy-Item -Path '\\vmware-host\Shared Folders\vmconfig\.ssh\*'  -Destination $Env:HomePath\.ssh\
 wget 'https://raw.githubusercontent.com/stephen-marshall-moore/windows-dev-box-setup-scripts/useconfig/config/chocoVM.config'  -OutFile $Env:HomePath\chocoVM.config
