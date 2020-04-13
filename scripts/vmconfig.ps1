6c5a94b2644bee92f4a0c496d6497e01ae2d3c50
stephen-marshall-moore <stephen.moore@linux.com>
2020-04-13 10:30:01 -0700

    Update vmconfig.ps1
    thinking there were duplications

--- scripts/vmconfig.ps1
+++ scripts/vmconfig.ps1
@@ -1,5 +1,4 @@
 Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\cylroot.pem'  -CertStoreLocation 'Cert:\LocalMachine\Root'
-Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\root_cylance.cer'  -CertStoreLocation 'Cert:\LocalMachine\Root'
 Import-Certificate -FilePath '\\vmware-host\Shared Folders\vmconfig\certs\star_cylance_com.cer'  -CertStoreLocation 'Cert:\LocalMachine\Root'
 New-Item -ItemType 'directory' -Path 'C:\Users\stephen\.ssh'
 Copy-Item -Path '\\vmware-host\Shared Folders\vmconfig\.ssh\*'  -Destination 'C:\Users\stephen\.ssh\'
